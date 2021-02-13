3bce13037dacdab493ee1ad57e5b5fb00a965439
EvandroRBR <evandro.braga01@fatec.sp.gov.br>
2021-02-13 00:49:54 -0300

    Usando Controllers

---
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+export default class UsersController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+
+    const createUser = container.resolve(CreateUserService);
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return res.json(user);
+  }
+
+  public async teste(req: Request, res: Response): Promise<Response> {
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
+
+    const user = await updateUserAvatar.execute({
+      user_id: req.user.id,
+      avatarFilename: req.file.filename,
+    });
+
+    delete user.password;
+
+    return res.json(user);
+  }
+}
